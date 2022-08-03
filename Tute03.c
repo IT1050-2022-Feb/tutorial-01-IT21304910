0d99d5c105ba656abe68fb2f43728d6d90eeaa91
IT21304910 <110513413+IT21304910@users.noreply.github.com>
2022-08-03 06:27:56 +0000

    exercise3

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,21 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
+int main() 
+{
+  float no,sum=0;
+  int i;
+
+  for(i=1;i<=5;i++)
+    {
+      printf("Next number : ");
+      scanf("%f",&no);
+      sum+=no;
+      
+    }
+  printf("The sum of 5 numbers %.2f", sum);
   
   return 0;
 }
+
 

672cba7298823fd5ac01a325f6b76cbd437a8e80
IT21304910 <110513413+IT21304910@users.noreply.github.com>
2022-08-03 08:10:49 +0000

    exercise4 is done

--- Tute04.c
+++ Tute04.c
@@ -3,17 +3,47 @@
 Implement the three functions minimum(), maximum() and multiply() below the main() function.
 
 Do not change the code given in the main() function when you are implementing your solution.*/
-
 #include <stdio.h>
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+int minimum(int no1,int  no2);
+int maximum(int no1, int no2);
+int multiply(int no1,int  no2);
+int main() 
+{
+	int min, max, mult,minimum,maximum,multiply;
+	int no1,no2;
+	 printf("Enter a value for no 1 : ");
+     scanf("%d", &no1);
+     printf("Enter a value for no 2 : ");
+     scanf("%d", &no2);
+	min = minimum(no1, no2);
+	max = maximum(no1, no2);
+	mult =  multiply(no1, no2);
+     printf("%d ", min);
+     printf("%d ", max);
+     printf("%d ",mult);
+
    return 0;
 }
+ int minimum(int no1,int  no2)
+{
+  if(no1<no2)
+  {
+    return minimum;
+    
+  }
+  }
+  int maximum(int no1,int  no2)
+  {
+    if(no1>no2)
+    {
+      retrun maximum;
+    }
+  }
+ int multiply(int no1,int  no2)
+{
+  multiply = no1*no2;
+  return multiply;
+}
+  
+
